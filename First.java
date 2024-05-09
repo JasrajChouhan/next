dea3356e68f7d94ed0a2111dc739e910ef83a94e
jaschouhan <jasrajc990@gmail.com>
2024-05-09 10:34:24 +0530

    first file

---
+++ First.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+public class First {
+    public static void insertionSort (int[] arr) {
+        // add your logic here
+        int j=0,temp=0;
+        for(int i=1;i<arr.length;i++){
+            temp=arr[i];
+            j=i-1;
+            while(j>=0 && arr[j]>temp){
+                arr[j+1]=arr[j];
+                j--;
+            }
+            arr[j+1]=temp;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        int a[] = {2,1,4,-9,50} ;
+        insertionSort(a);
+    }
+}
